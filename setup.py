9f0be32e6a09f4380378e1d9f20d1a9afd06b163
Sarthak Dehadray <sarthakdehadray@gmail.com>
2023-05-03 08:15:52 +0530

    utils and logger added

--- setup.py
+++ setup.py
@@ -12,18 +12,17 @@ SRC_REPO = "CNN_Classifier"
 AUTHOR_EMAIL = "sarthakdehadray@gmail.com"
 
 setuptools.setup(
-    name = SRC_REPO,
-    version = __version__,
-    author = AUTHOR_USER_NAME,
-    author_email = AUTHOR_EMAIL,
-    description = "A python package for CNN app",
-    long_description = long_description,
-    long_description_content = "/text/markdown",
-    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_url = {
-        "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir = {"":"src"},
-    packages = setuptools.find_packages(where = "src")
-
+    package_dir = {"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
